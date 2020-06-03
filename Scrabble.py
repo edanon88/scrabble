22936355e09ded7eb414e04d5d8982c7b76b87f1
EAFD88 <edd.danon@gmail.com>
2020-06-03 11:38:31 +0100

    Added word scoring function and quick user input test

--- Scrabble.py
+++ Scrabble.py
@@ -2,3 +2,18 @@
 
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+# Creating points dictionary and adding blank tile
+letters_to_points = {letter:value for letter,value in zip(letters,points)}
+letters_to_points[" "]=0
+
+# Function to calculate word score
+def score_word(word):
+	point_total=0
+	for letter in word:
+		point_total += letters_to_points[letter.upper()]
+	return point_total
+
+# Testing
+user_word=input ("Word: ")
+print("Score: " + str(score_word(user_word)))
